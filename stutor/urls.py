baa86e9591ce20cbf29ecec4131f1f96cd5d3763
SethCardoen <seth.cardoen@gmail.com>
2021-06-30 18:39:30 +0200

    Tutor page basic

--- stutor/urls.py
+++ stutor/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('student/', views.student_page),
-    path('tutor/<str:pk_tutor>/', views.tutor_page),
+    path('', views.home, name="home"),
+    path('student/', views.student_page, name="student"),
+    path('tutor/<str:pk_tutor>/', views.tutor_page, name="tutor"),
 
 ]
