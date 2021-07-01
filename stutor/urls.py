4521cff56cf6a3455e8aebaa8b180a0999dac928
SethCardoen <seth.cardoen@gmail.com>
2021-07-01 09:25:36 +0200

    Dashboard tables, view page buttons navbar buttons functional --> ink to student and tutor page

--- stutor/urls.py
+++ stutor/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('student/', views.student_page, name="student"),
-    path('tutor/<str:pk_tutor>/', views.tutor_page, name="tutor"),
+    path('', views.home, name='home'),
+    path('student/', views.student_page, name='student'),
+    path('tutor/<str:pk_tutor>/', views.tutor_page, name='tutor'),
 
 ]
